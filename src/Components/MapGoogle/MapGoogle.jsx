fdf1f2973f3086379ad7170e3cade7d3c5f5c8d1
Vitor Leal <vitorujleal@gmail.com>
2024-03-08 02:48:06 -0300

    add render google maps search city

---
+++ src/Components/MapGoogle/MapGoogle.jsx
@@ -0,0 +1,53 @@
+import getCityCoordinates from "../../api/getCityCoordinates";
+import React, { useState, useEffect, useMemo } from "react";
+import GoogleMapReact from 'google-map-react'
+
+const MAP_KEY = "AIzaSyCPYqhsdXgumUpVJBwvNmLQvLQ2l9r9ofg";
+
+const MapGoogle = ({ searchedCity }) => {
+  const [coordinates, setCoordinates] = useState(null);
+
+  useEffect(() => {
+    const loadCoordinates = async () => {
+      try {
+        const coords = await getCityCoordinates(searchedCity);
+        setCoordinates({
+          lat: coords.lat,
+          lng: coords.lon
+        });
+      } catch (error) {
+        console.error('Erro ao carregar coordenadas:', error.message);
+        setCoordinates(null);
+      }
+    };
+
+    if (searchedCity) {
+      loadCoordinates();
+    } else {
+      setCoordinates(null);
+    }
+  }, [searchedCity]);
+
+
+  const defaultProps = useMemo(() => {
+    return {
+      defaultCenter: { lat: 0, lng: 0 },
+      defaultZoom: 11
+    };
+  }, []);
+
+  return (
+    <div style={{ height: '400px', width: '700px' }}>
+      {coordinates && (
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: MAP_KEY }}
+          center={coordinates}
+          {...defaultProps}
+        >
+        </GoogleMapReact>
+      )}
+    </div>
+  );
+};
+
+export default MapGoogle;
